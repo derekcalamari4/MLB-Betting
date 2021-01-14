56b83b7f69c1200a76f9ea728f5cd245dab5f7cb
Derek Calamari <derek.calamari@gmail.com>
2021-01-14 15:22:14 -0600

    console / comparison output

--- winner.py
+++ winner.py
@@ -1,10 +1,13 @@
 import weight2020
+import effiency
+
 
 teams = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'DET', 'HOU', 'KCR',
          'LAA', 'LAD', 'MIA', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK', 'PHI', 'PIT', 'SDP', 'SEA',
          'SFG', 'STL', 'TBR', 'TEX', 'TOR', 'WSN']
 stat_abb3 = {'H': 0, 'R': 1, 'R/G': 2, 'HR': 3, 'RBI': 4, 'OBP': 5, 'SLG': 6, 'OPS': 7}
 stat_abb4 = {'RA/G': 0, 'ERA': 1, 'WHIP': 2, 'SO/W': 3, 'BB/9': 4}
+
 
 '''
 console comparison
@@ -42,16 +45,31 @@ def get_comparison():
     if teama not in teams or teamb not in teams:
         print('Enter valid abbreviations for MLB teams')
     weights = compare_weights(teama, teamb)
+    a_total = 0
+    b_total = 0
     if weights[0] > weights[1]:
+        a_total += 1
         print('OFF: ' + teama)
     else:
+        b_total += 1
         print('OFF: ' + teamb)
     if weights[2] > weights[3]:
+        a_total += 1
         print('DEF: ' + teama)
     else:
+        b_total += 1
         print('DEF: ' + teamb)
     if weights[4] > weights[5]:
+        a_total += 1
         print('WAR: ' + teama)
     else:
+        b_total += 1
         print('WAR: ' + teamb)
-get_comparison()
+    print(teama + ' OFF EFF: ' + str(effiency.off_eff(teama)) + ' %')
+    print(teama + ' DEF EFF: ' + str(effiency.def_eff(teama)) + ' %')
+    print(teamb + ' OFF EFF: ' + str(effiency.off_eff(teamb)) + ' %')
+    print(teamb + ' DEF EFF: ' + str(effiency.def_eff(teamb)) + ' %')
+    if a_total > b_total:
+        print('PROJ WIN: ' + teama)
+    elif b_total > a_total:
+        print('PROJ WIN: ' + teamb)
