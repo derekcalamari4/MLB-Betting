fa3316f7bfff631d4649e26b9f4d987f27c6e1ba
Derek Calamari <derek.calamari@gmail.com>
2021-01-25 11:57:07 -0600

    sim 2020 mlb szn to check accuracy of statistical weighting

--- sim2020.py
+++ sim2020.py
@@ -1,6 +1,5 @@
 import winner
 import parseresults2020
-import weight2
 
 teams = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'DET', 'HOU', 'KCR',
          'LAA', 'LAD', 'MIA', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK', 'PHI', 'PIT', 'SDP', 'SEA',
@@ -22,21 +21,6 @@ def sim2020_weight1():
             if game_winner == opponents[i] and result == 0:
                 count += 1
         return count / total_games
-print(sim2020_weight1())
 
-def sim2020_weight2():
-    for team in teams:
-        count = 0
-        total_games = 0
-        opponents = parseresults2020.get_opp(team)
-        results = parseresults2020.get_results(team)
-        for i in range(opponents.__len__()):
-            game_winner = weight2.compare_teams(team, opponents[i])
-            result = results[i]
-            total_games += 1
-            if game_winner == team and result == 1:
-                count += 1
-            elif game_winner == opponents[i] and result == 0:
-                count += 1
-    return count / total_games
-print(sim2020_weight2())
+
+print('Weighting accuracy for 2020 mlb szn: ' + str(round(sim2020_weight1()), 3) + '%')
